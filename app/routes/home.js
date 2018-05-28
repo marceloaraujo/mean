1b041c20a14836a05a902933cf11423534ea6548
Marcelo Araujo <marcelo.araujo@loupen.com.br>
2018-05-28 15:31:05 -0300

    Add express-load and body-parser to test a post method

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,8 +2,14 @@
  * Uma rota é necessária para acessar o arquivo index.ejs dentro da pasta views,
  * ou seja, precisamos tornar publico um caminho de acesso para essa página
  */
-var controller = require('../controllers/home')(); // os parenteses no final foram necessários para corrigir o seguinte erro: Route.get() requires a callback function but got a [object Undefined]
+// var controller = require('../controllers/home')(); // os parenteses no final foram necessários para corrigir o seguinte erro: Route.get() requires a callback function but got a [object Undefined]
 module.exports = function(app) {
+    /**
+     * Através do express-load, é possível instanciar o controller da seguinte forma: app.controllers.home,
+     * sem a necessidade da função 'require';
+     */
+    var controller = app.controllers.home;
+
     /**
      * Vamos responder à url '/'
      * Configurando a rota para que chame o controller
--- config/express.js
+++ config/express.js
@@ -5,6 +5,9 @@
  */
 var express = require('express');
 var home = require('../app/routes/home');
+var bodyParser = require('body-parser');
+
+var load = require('express-load');
 
 module.exports = function() {
     var app = express();
@@ -23,7 +26,61 @@ module.exports = function() {
     /**
      * Configurando a rota 'home', que escutará a requisição das url's 'index' e '/'
      */
-    home(app);
+    // home(app);
+
+    /**
+     * Usando o express-load para carregar as dependências
+     * A função load carregará tudo dentro das pastas app/models, app/routes e app/controllers
+     * É necessário seguir essa ordem de carregamento, senão não conseguiremos acessar os controllers
+     * dentro das rotas, se os controllers forem carregados por último
+     * O parâmetro cwd serve para mudar o diretório padrão do load. Sem isso, o load iria produrar na pasta 
+     * raiz do projeto (contatooh), e precisamos que ele procure a partir da pasta contatooh/app
+     */
+    load('models', { cwd: 'app' })
+        .then('controllers')
+        .then('routes')
+        .into(app);
+
+
+    /**
+     * Configurando o app para usar bodyParser
+     * Isso permitirá pegar os dados de um post 
+     */    
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+    /**
+     * TESTE: criando uma rota para a api
+     *
+    var router = express.Router();
+
+    **
+     * Testando a rota com um get
+     *
+    router.get('/teste', function(req, res) {
+        res.json({message: 'Bem-vindo à api de teste!'});
+    });
+
+    /**
+     * Testando a rota com um post
+     *
+    router.route('/loupen/:empresa/:id_integracao').post(function(req, res) {
+        console.log(req.body);
+        var empresa = req.params.empresa;
+        var idIntegracao = req.params.id_integracao;
+
+        if(empresa == "teste" && idIntegracao == 1) {
+            res.json({message: 'POST efetuado!'});
+        } else {
+            res.json({message: 'Dados não encontrados!!'});
+        }
+    });
+
+    /**
+     * Registrando a rota
+     *
+    app.use('/api', router);
+    */
 
     /**
      * Usando ejs para criar templates
